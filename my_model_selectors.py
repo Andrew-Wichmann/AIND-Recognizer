8043333f50474357c4b7fd794166df4509cf0caf
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-12-07 14:38:37 -0600

    haha, did I say MIN? I meant MAX! haha, oh boy...

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -105,15 +105,15 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        minL = float('inf')
+        maxL = -float('inf')
         best_model = None
         for n_hidden in range(self.min_n_components, self.max_n_components+1):
             try:
                 hmm_model = GaussianHMM(n_components=n_hidden, covariance_type="diag", n_iter=1000,
                                         random_state=self.random_state, verbose=self.verbose).fit(self.X, self.lengths)
                 logL = hmm_model.score(self.X, self.lengths)
-                if logL < minL:
-                    minL = logL
+                if logL > maxL:
+                    maxL = logL
                     best_model = hmm_model
             except:
                 import sys
