fdf4a6c5e1d745b797e20e568bc8c54dffde006e
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-12-08 17:18:52 -0600

    I... I think I did it...

--- my_recognizer.py
+++ my_recognizer.py
@@ -1,7 +1,6 @@
 import warnings
 from asl_data import SinglesData
 
-
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
 
@@ -17,9 +16,21 @@ def recognize(models: dict, test_set: SinglesData):
        guesses is a list of the best guess words ordered by the test set word_id
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
+    # import pudb;pudb.set_trace()
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    for key in test_set.get_all_Xlengths():
+        best_guess = None 
+        best_prob = float('-inf')
+        prob_for_this_word = {}
+        for word in models:
+            if models[word] != None:
+                prob = models[word].score(test_set.get_all_Xlengths()[key][0], test_set.get_all_Xlengths()[key][1])
+                prob_for_this_word[word] = prob
+                if prob > best_prob:
+                    best_prob = prob
+                    best_guess = word
+        probabilities.append(prob_for_this_word)
+        guesses.append(best_guess)
+    return probabilities, guesses
